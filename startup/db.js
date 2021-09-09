0486835874215edb7427c53a69f1fdc46f583c36
Raj Kumar Kausik <rrajkukausik@gmail.com>
2021-09-09 12:38:54 +0530

    url

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = function () {
   //const DB_URL=config.get("DB_URL"); // Use this when DB_URL is present in environment
   // const DB_URL="mongodb://interactive-learning-mongo-service:27017/local" // Use this when MongoDB is running in docker
-  const DB_URL = "mongodb://localhost:27017/WordRaceDB"; //Use this when MongoDB is running in your local system
+  const DB_URL = "mongodb+srv://mindfire:mindfire@cluster0.s3ogo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //Use this when MongoDB is running in your local system
 
   mongoose
     .connect(DB_URL, {
